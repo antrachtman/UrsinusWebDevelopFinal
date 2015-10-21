36ff0cf11cad670e3782a6e70ebb9bd05b50c352
Andrew Trachtman <antrachtman@users.noreply.github.com>
2015-10-21 14:52:07 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php //login.php
-$db_hostname = '127.0.0.1';
-$db_database = 'dakidozer';
-$db_username = 'root';
+$db_hostname = '';
+$db_database = '';
+$db_username = '';
 $db_password = '';
 session_start();
 //pass: Tm3ZaR4ZUHDEUsK3 phpmyadmin
